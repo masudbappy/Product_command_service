6f35bbc469c9d9e4542cd2eb5960a03f27fdd17e
masudbappy <masud.bappy90@gmail.com>
2025-02-07 20:44:58 +0600

    set up project

---
+++ src/main/java/com/ecommerce/product_command_service/ProductCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.product_command_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCommandServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/product_command_service/ProductCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.product_command_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
