42334a8e37ebd8f2c42532654bf64d0ab317e2fc
ShartazFeeham <mdfeeham@gmail.com>
2025-02-07 21:55:40 +0600

    [pcs] updated docker compose & application config

--- src/main/java/com/ecommerce/product_command_service/CommandApp.java
+++ src/main/java/com/ecommerce/product_command_service/CommandApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductCommandServiceApplication {
+public class CommandApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductCommandServiceApplication.class, args);
+		SpringApplication.run(CommandApp.class, args);
 	}
 
 }
--- src/test/java/com/ecommerce/product_command_service/CommandAppTests.java
+++ src/test/java/com/ecommerce/product_command_service/CommandAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductCommandServiceApplicationTests {
+class CommandAppTests {
 
 	@Test
 	void contextLoads() {
