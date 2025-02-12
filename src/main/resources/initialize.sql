dd980c6f4a08fbdd493d5161517e0d79caa6cf3c
ShartazFeeham <mdfeeham@gmail.com>
2025-02-12 20:14:00 +0600

    [pcs] Completed initialization database queries

--- src/main/resources/initialize.sql
+++ src/main/resources/initialize.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS variant;
-DROP TABLE IF EXISTS customizable_property;
-DROP TABLE IF EXISTS product_tags;
-DROP TABLE IF EXISTS tags;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS shop;
-DROP TABLE IF EXISTS sub_category;
-DROP TABLE IF EXISTS category;
-DROP TYPE product_status;
+DROP TABLE IF EXISTS variants CASCADE;
+DROP TABLE IF EXISTS product_tags CASCADE;
+DROP TABLE IF EXISTS customizable_property CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS shop CASCADE;
+DROP TABLE IF EXISTS sub_category CASCADE;
+DROP TABLE IF EXISTS category CASCADE;
+DROP TABLE IF EXISTS tags CASCADE;
+DROP TYPE IF EXISTS product_status;
 
 -----------------------------------------------------------------
 -- Category table
@@ -17,6 +17,11 @@ CREATE TABLE category (
                           updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX idx_category_name ON category(id);
+CREATE INDEX idx_category_name2 ON category(id);
+CREATE INDEX idx_category_name3 ON category(id);
+CREATE INDEX idx_category_name4 ON category(id);
+
+INSERT INTO category(name) VALUES ('test');
 
 -----------------------------------------------------------------
 -- Sub category table
@@ -25,11 +30,10 @@ CREATE TABLE sub_category (
                               name VARCHAR(255) NOT NULL UNIQUE CHECK(name = lower(name) AND length(name) >= 3),
                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                               updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
     -- Foreign key references
                               category_id INTEGER REFERENCES category(id) NOT NULL
 );
-CREATE INDEX idx_sub_category_name ON sub_category(name);
+CREATE INDEX idx_sub_category_name_1 ON sub_category(name);
 
 -----------------------------------------------------------------
 -- Shop table
@@ -77,17 +81,37 @@ CREATE INDEX "idx_tags_id" ON tags(id);
 
 -----------------------------------------------------------------
 -- Variant table
+CREATE TABLE variants(
+                         id BIGSERIAL PRIMARY KEY,
+                         name VARCHAR(30) CHECK(length(name) >=2),
+                         quantity INTEGER DEFAULT 0 CHECK(quantity >= 0),
+                         price DOUBLE PRECISION NOT NULL CHECK(price >= 0),
+
+    -- Foreign key
+                         product_id BIGINT REFERENCES products(id)
+);
+CREATE INDEX "idx_variant_id" ON variants USING HASH(id);
+CREATE INDEX "idx_variant_product_id" ON variants(product_id);
 
 -----------------------------------------------------------------
 -- Customizable property table
+CREATE TABLE customizable_property(
+                                      id BIGSERIAL PRIMARY KEY,
+                                      param_name VARCHAR(255) NOT NULL,
+                                      param_value VARCHAR(255) NOT NULL,
 
------------------------------------------------------------------
--- Variant + product table
+    -- Foreign key
+                                      product_id BIGINT REFERENCES products(id)
+);
+CREATE INDEX "idx_customizable_property_product_id" ON customizable_property USING HASH(product_id);
 
 -----------------------------------------------------------------
 -- Tag + product table
+CREATE TABLE product_tag(
+                            id BIGSERIAL PRIMARY KEY,
 
------------------------------------------------------------------
--- Customizable property + product table
-
-
+    -- Foreign key
+                            product_id BIGINT REFERENCES products(id),
+                            tag_id BIGINT REFERENCES tags(id)
+);
+CREATE INDEX "idx_product_tag_product_id" ON product_tag USING HASH(product_id);
